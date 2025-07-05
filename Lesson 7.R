7b58f4dab42a1e1168353d151d319d2b4cb76f64
galdovaldonavas <egonzalezcabanes@gmail.com>
2025-07-05 16:16:58 -0700

    Start

---
+++ Lesson 7.R
@@ -0,0 +1,11 @@
+library(BayesFactor) # for bayesian analyses
+library(car) # to explore pair scatterplot matrices, data explorations,  box-cox transformations
+library(corrplot)# for correlation graphs
+library(gplots)# for including colors in correlation graphs
+library(lattice)# for segment visualizations
+library(binom)# for confidence intervals in binomial tests
+library(multcomp) # for plotting the means and ci of multigroup comparisons
+library(ggformula) # also for plots
+
+sat
+
